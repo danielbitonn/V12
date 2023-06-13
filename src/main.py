7f82d1031c432ac0f79fc40344997ef380975927
Daniel Office <danielbitonn@gmail.com>
2023-06-14 00:09:01 +0300

    start to build ui

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
+from src.util.win_creator import *
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -10,4 +9,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    createWin()
 
---
+++ src/util/buttons.py
@@ -0,0 +1,5 @@
+def but1_clicked():
+    print("Button 1 clicked")
+
+def but2_clicked():
+    print("Button 2 clicked")
